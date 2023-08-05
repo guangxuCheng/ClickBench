9bacc1330bd033efb4c715ee6406d5e773487ef5
andrewcheng <andrewcheng@tencent.com>
2023-08-06 01:06:23 +0800

    add perf shell

---
+++ starrocks/get_perf_10s.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+if [ "$#" -ne 2 ]; then
+    echo "Usage: $0 <pid> <query_index>"
+    exit 1
+fi
+
+pid=$1
+query_index=$2
+
+script_dir=$(dirname "$0")
+queries_file="$script_dir/queries.sql"
+mapfile -t sql_queries < "$queries_file"
+
+selected_query=${sql_queries[$((query_index - 1))]}
+
+perf record -e cpu-clock -g -p $pid &
+perf_pid=$!
+
+duration=10
+end_time=$((SECONDS + duration))
+
+while [ $SECONDS -lt $end_time ]
+do
+    mysql -h 127.0.0.1 -P9030 -uroot -D hits -e "$selected_query"
+done
+
+kill -INT $perf_pid
+
+wait $perf_pid
+
+exit_status=$?
+
+if [ $exit_status -ne 0 ] && [ $exit_status -ne 130 ]; then
+    echo "Error: 'perf record' command did not terminate correctly"
+    exit 1
+fi
+
+perf script -i perf.data &> perf.unfold
+./FlameGraph/stackcollapse-perf.pl perf.unfold &> perf.folded
+./FlameGraph/flamegraph.pl perf.folded > perf.svg
+
+mv perf.svg clickbench_${query_index}.svg
+
+rm perf.data perf.unfold perf.folded
---
+++ starrocks/run_perf.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# 获取入参
+a=$1
+b=$2
+
+if [ ! -d "FlameGraph" ]; then
+  git clone https://github.com/brendangregg/FlameGraph.git
+fi
+
+pid=$(ps aux | grep starrocks_be | grep -v grep | awk '{print $2}')
+
+# 执行 get_perf_10s.sh 脚本 b-a 次
+for ((i=$a; i<=$b; i++))
+do
+  ./get_perf_10s.sh $pid $i
+done
