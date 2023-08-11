545828259862a5787a33ae2bcbfabfef218ec42f
andrewcheng <andrewcheng@tencent.com>
2023-08-11 14:10:36 +0800

    update hits.tsv

--- starrocks/benchmark.sh
+++ starrocks/benchmark.sh
@@ -47,8 +47,8 @@ cd ../
 if [ ! -f "/home/ec2-user/hits.tsv" ]; then
     wget --no-verbose --continue 'https://datasets.clickhouse.com/hits_compatible/hits.tsv.gz'
     gzip -d hits.tsv.gz
-else
-    cp /home/ec2-user/hits.tsv .
+#else
+#    cp /home/ec2-user/hits.tsv .
 fi
 
 # Create Table
@@ -60,7 +60,7 @@ START=$(date +%s)
 echo "Start to load data..."
 result=`curl --location-trusted \
     -u root: \
-    -T "hits.tsv" \
+    -T "/home/ec2-user/hits.tsv" \
     -H "label:hits_tsv_${START}" \
     -H "columns: WatchID,JavaEnable,Title,GoodEvent,EventTime,EventDate,CounterID,ClientIP,RegionID,UserID,CounterClass,OS,UserAgent,URL,Referer,IsRefresh,RefererCategoryID,RefererRegionID,URLCategoryID,URLRegionID,ResolutionWidth,ResolutionHeight,ResolutionDepth,FlashMajor,FlashMinor,FlashMinor2,NetMajor,NetMinor,UserAgentMajor,UserAgentMinor,CookieEnable,JavascriptEnable,IsMobile,MobilePhone,MobilePhoneModel,Params,IPNetworkID,TraficSourceID,SearchEngineID,SearchPhrase,AdvEngineID,IsArtifical,WindowClientWidth,WindowClientHeight,ClientTimeZone,ClientEventTime,SilverlightVersion1,SilverlightVersion2,SilverlightVersion3,SilverlightVersion4,PageCharset,CodeVersion,IsLink,IsDownload,IsNotBounce,FUniqID,OriginalURL,HID,IsOldCounter,IsEvent,IsParameter,DontCountHits,WithHash,HitColor,LocalEventTime,Age,Sex,Income,Interests,Robotness,RemoteIP,WindowName,OpenerName,HistoryLength,BrowserLanguage,BrowserCountry,SocialNetwork,SocialAction,HTTPError,SendTiming,DNSTiming,ConnectTiming,ResponseStartTiming,ResponseEndTiming,FetchTiming,SocialSourceNetworkID,SocialSourcePage,ParamPrice,ParamOrderID,ParamCurrency,ParamCurrencyID,OpenstatServiceName,OpenstatCampaignID,OpenstatAdID,OpenstatSourceID,UTMSource,UTMMedium,UTMCampaign,UTMContent,UTMTerm,FromTag,HasGCLID,RefererHash,URLHash,CLID" \
     http://localhost:8030/api/hits/hits/_stream_load`
