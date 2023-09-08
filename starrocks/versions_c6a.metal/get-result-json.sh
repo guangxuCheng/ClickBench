805f02508ca02663af02f55540564c2250cf64f5
andrewcheng <andrewcheng@tencent.com>
2023-09-08 14:01:57 +0800

    add c6a.metal

---
+++ starrocks/versions_c6a.metal/get-result-json.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+VERSION=$1
+# set -x
+if [[ ! -d results ]]; then mkdir results; fi
+
+echo -e "{
+    \"version\": \"$VERSION\",
+    \"result\": [
+$(
+    r=$(sed -r -e 's/query[0-9]+,/[/; s/$/],/' ../result.csv)
+    echo "${r%?}"
+)
+    ]
+}
+" | tee results/"$VERSION.json"
+
